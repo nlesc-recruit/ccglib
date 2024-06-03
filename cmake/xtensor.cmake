c68e84ed9b054f7824413998ccede950afacdb4e
Bram Veenboer <bram.veenboer@gmail.com>
2024-06-03 16:42:45 +0200

    Add guard around xtl and xtensor in CMake

--- cmake/xtensor.cmake
+++ cmake/xtensor.cmake
@@ -1,22 +1,24 @@
-FetchContent_Declare(
-  xtl
-  GIT_REPOSITORY https://github.com/xtensor-stack/xtl.git
-  GIT_TAG 0.7.7
-  GIT_SHALLOW TRUE)
-FetchContent_Populate(xtl)
+if(NOT TARGET xtl)
+  FetchContent_Declare(
+    xtl
+    GIT_REPOSITORY https://github.com/xtensor-stack/xtl.git
+    GIT_TAG 0.7.7
+    GIT_SHALLOW TRUE)
+  FetchContent_Populate(xtl)
+  add_library(xtl INTERFACE)
+  target_include_directories(
+    xtl SYSTEM INTERFACE "${FETCHCONTENT_BASE_DIR}/xtl-src/include")
+endif()
 
-FetchContent_Declare(
-  xtensor
-  GIT_REPOSITORY https://github.com/xtensor-stack/xtensor.git
-  GIT_TAG 0.25.0
-  GIT_SHALLOW TRUE)
-FetchContent_Populate(xtensor)
-
-add_library(xtl INTERFACE)
-target_include_directories(xtl SYSTEM
-                           INTERFACE "${FETCHCONTENT_BASE_DIR}/xtl-src/include")
-
-add_library(xtensor INTERFACE)
-target_include_directories(
-  xtensor SYSTEM INTERFACE "${FETCHCONTENT_BASE_DIR}/xtensor-src/include")
-target_link_libraries(xtensor INTERFACE xtl)
+if(NOT TARGET xtensor)
+  FetchContent_Declare(
+    xtensor
+    GIT_REPOSITORY https://github.com/xtensor-stack/xtensor.git
+    GIT_TAG 0.25.0
+    GIT_SHALLOW TRUE)
+  FetchContent_Populate(xtensor)
+  add_library(xtensor INTERFACE)
+  target_include_directories(
+    xtensor SYSTEM INTERFACE "${FETCHCONTENT_BASE_DIR}/xtensor-src/include")
+  target_link_libraries(xtensor INTERFACE xtl)
+endif()
