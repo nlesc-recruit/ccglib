ce0699f292df427f4d723c55777f10a592033b88
Ronald Rook <ronald.rook@sioux.eu>
2024-08-27 14:24:38 +0000

    Replaced FetchContent_Populate for xtl and xtensor

--- cmake/xtensor.cmake
+++ cmake/xtensor.cmake
@@ -3,11 +3,9 @@ if(NOT TARGET xtl)
     xtl
     GIT_REPOSITORY https://github.com/xtensor-stack/xtl.git
     GIT_TAG 0.7.7
-    GIT_SHALLOW TRUE)
-  FetchContent_Populate(xtl)
-  add_library(xtl INTERFACE)
-  target_include_directories(
-    xtl SYSTEM INTERFACE "${FETCHCONTENT_BASE_DIR}/xtl-src/include")
+    GIT_SHALLOW TRUE
+    EXCLUDE_FROM_ALL)
+  FetchContent_MakeAvailable(xtl)
 endif()
 
 if(NOT TARGET xtensor)
@@ -15,10 +13,7 @@ if(NOT TARGET xtensor)
     xtensor
     GIT_REPOSITORY https://github.com/xtensor-stack/xtensor.git
     GIT_TAG 0.25.0
-    GIT_SHALLOW TRUE)
-  FetchContent_Populate(xtensor)
-  add_library(xtensor INTERFACE)
-  target_include_directories(
-    xtensor SYSTEM INTERFACE "${FETCHCONTENT_BASE_DIR}/xtensor-src/include")
-  target_link_libraries(xtensor INTERFACE xtl)
+    GIT_SHALLOW TRUE
+    EXCLUDE_FROM_ALL)
+  FetchContent_MakeAvailable(xtensor)
 endif()
