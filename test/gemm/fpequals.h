abfb3f1d0f368c506652294a824550cdf5136101
Leon Oostrum <l.oostrum@esciencecenter.nl>
2025-06-24 16:09:45 +0200

    Add support for bfloat16

--- test/gemm/fpequals.h
+++ test/gemm/fpequals.h
@@ -6,6 +6,7 @@
 #include <limits>
 
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <ccglib/bf16.h>
 #include <ccglib/fp16.h>
 
 namespace ccglib::test {
@@ -17,6 +18,12 @@ template <typename T> constexpr float getEpsilon() {
     // tf32 uses the same mantissa as the half-precision math
     // the precision for normal numbers is therefore 2^-10
     return 0.000976562;
+  } else if constexpr (std::is_same_v<T, bf16>) {
+    // bfloat16 uses an 8-bit mantissa (of which 7 bits are stored)
+    // the precision for normal numbers is therefore 2^-7
+    // Note: this epsilon exists already in e.g. HIP, but is not constexpr so
+    // cannot be reused here
+    return 0.0078125;
   }
   return std::numeric_limits<T>::epsilon();
 }
@@ -24,7 +31,16 @@ template <typename T> constexpr float getEpsilon() {
 template <typename T> void fpEquals(T x, T y, size_t K = 1) {
   constexpr float epsilon = getEpsilon<T>();
 
-  if constexpr (std::is_same_v<T, half>) {
+  if constexpr (std::is_same_v<T, bf16>) {
+    // We need to upcast since Catch2 cannot print bfloat16 types in case of
+    // test failure.
+    const float x_conv = static_cast<float>(x);
+    const float y_conv = static_cast<float>(y);
+
+    // We are more lenient in WithinAbs since we use a less precise type.
+    REQUIRE_THAT(y_conv, Catch::Matchers::WithinAbs(x_conv, epsilon * K) ||
+                             Catch::Matchers::WithinRel(x_conv, epsilon * 100));
+  } else if constexpr (std::is_same_v<T, half>) {
     // We need to upcast since Catch2 cannot print float16/half types in case of
     // test failure.
     const float x_conv = __half2float(x);
