2f44b3a661c99fda1fa6983dff47f0c637fc103a
Leon Oostrum <l.oostrum@esciencecenter.nl>
2025-09-25 11:37:31 +0200

    Add complex axis to C_shared; preparation for alpha/beta scaling in store_matrix_padded

--- kernels/matrix_operations.h
+++ kernels/matrix_operations.h
@@ -103,44 +103,50 @@ store_matrix_padded(Accumulator_t sum[COMPLEX][M_PER_WARP / M_PER_WMMA]
                     const size_t &warpN, const size_t &M_TILES,
                     const size_t &N_TILES) {
 
-#if defined(C_COMPLEX_PLANAR)
-  for (size_t c = 0; c < COMPLEX; c++) {
-    for (size_t m = 0; m < M_TILES; m++) {
-      for (size_t n = 0; n < N_TILES; n++) {
-#else
   for (size_t m = 0; m < M_TILES; m++) {
     for (size_t n = 0; n < N_TILES; n++) {
-      for (size_t c = 0; c < COMPLEX; c++) {
-#endif
-        wmma::store_matrix_sync(&C_s[warpM][warpN][0][0], sum[c][m][n],
-                                N_PER_WMMA, wmma::mem_row_major);
+      wmma::store_matrix_sync(&C_s[REAL][warpM][warpN][0][0], sum[REAL][m][n],
+                              N_PER_WMMA, wmma::mem_row_major);
+      wmma::store_matrix_sync(&C_s[IMAG][warpM][warpN][0][0], sum[IMAG][m][n],
+                              N_PER_WMMA, wmma::mem_row_major);
 
-        __syncwarp();
-        size_t m_index = global_idx_m(blockM, warpM, m);
-        size_t n_index = global_idx_n(blockN, warpN, n);
-        for (size_t t = threadIdx.x; t < M_PER_WMMA * N_PER_WMMA;
-             t += WARP_SIZE) {
-          size_t i = t / N_PER_WMMA;
-          size_t j = t % N_PER_WMMA;
-          // store the submatrix, padded values are set to zero
-          if (m_index + i < M_GLOBAL && n_index + j < N_GLOBAL) {
+      __syncwarp();
+      size_t m_index = global_idx_m(blockM, warpM, m);
+      size_t n_index = global_idx_n(blockN, warpN, n);
+      for (size_t t = threadIdx.x; t < M_PER_WMMA * N_PER_WMMA;
+           t += WARP_SIZE) {
+        size_t i = t / N_PER_WMMA;
+        size_t j = t % N_PER_WMMA;
+        // store the submatrix, padded values are set to zero
+        if (m_index + i < M_GLOBAL && n_index + j < N_GLOBAL) {
 #if defined(C_ROW_MAJOR)
 #if defined(C_COMPLEX_PLANAR)
-            C[batch][c][m_index + i][n_index + j] = C_s[warpM][warpN][i][j];
+          C[batch][REAL][m_index + i][n_index + j] =
+              C_s[REAL][warpM][warpN][i][j];
+          C[batch][IMAG][m_index + i][n_index + j] =
+              C_s[IMAG][warpM][warpN][i][j];
 #else
-            C[batch][m_index + i][n_index + j][c] = C_s[warpM][warpN][i][j];
+          C[batch][m_index + i][n_index + j][REAL] =
+              C_s[REAL][warpM][warpN][i][j];
+          C[batch][m_index + i][n_index + j][IMAG] =
+              C_s[IMAG][warpM][warpN][i][j];
 #endif
 #else
 #if defined(C_COMPLEX_PLANAR)
-            C[batch][c][n_index + j][m_index + i] = C_s[warpM][warpN][i][j];
+          C[batch][REAL][n_index + j][m_index + i] =
+              C_s[REAL][warpM][warpN][i][j];
+          C[batch][IMAG][n_index + j][m_index + i] =
+              C_s[IMAG][warpM][warpN][i][j];
 #else
-            C[batch][n_index + j][m_index + i][c] = C_s[warpM][warpN][i][j];
+          C[batch][n_index + j][m_index + i][REAL] =
+              C_s[IMAG][warpM][warpN][i][j];
+          C[batch][n_index + j][m_index + i][IMAG] =
+              C_s[IMAG][warpM][warpN][i][j];
 #endif
 #endif
-          }
         }
-        __syncwarp();
       }
+      __syncwarp();
     }
   }
 }
--- kernels/type_selector.h
+++ kernels/type_selector.h
@@ -212,7 +212,7 @@ constexpr size_t A_s_size =
 constexpr size_t B_s_size =
     NBUFFER * COMPLEX * N_PER_BLOCK * K_PER_WMMA / DeviceTraits::PACKING_FACTOR;
 
-constexpr size_t C_s_size = (M_PER_BLOCK / M_PER_WARP) *
+constexpr size_t C_s_size = COMPLEX * (M_PER_BLOCK / M_PER_WARP) *
                             (N_PER_BLOCK / N_PER_WARP) * M_PER_WMMA *
                             N_PER_WMMA;
 static_assert((A_s_size + B_s_size) * sizeof(Tin) >= C_s_size * sizeof(Tout),
